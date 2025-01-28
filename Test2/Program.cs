b957f7fafa52a7aa7f01b49ac570d100b5d8bb5f
Toxix98 <128359759+Toxix98@users.noreply.github.com>
2025-01-28 13:50:15 +0330

    Updating project to .net9

---
+++ Test2/Program.cs
@@ -0,0 +1,16 @@
+﻿using ProcessCapture;
+using ProcessCapture.Services;
+using ProcessCapture.Services.Interface;
+
+MonitoringService monitoringService = new MonitoringService();
+
+var dic = new Dictionary<string, string>();
+
+var result = await monitoringService.StartCaptureAsync("chrome");
+foreach (var kvp in result)
+{
+    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+    dic.Add(kvp.Key, kvp.Value);
+}
+
+Console.ReadKey();
